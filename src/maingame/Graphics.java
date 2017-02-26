ab56eff15e8257dcb0cd079fd59b518f38363e01
VayuDev <timo.habighorst@gmail.com>
2017-02-26 21:15:15 +0100

    Ein Beispiel für Graphics!

---
+++ src/maingame/Graphics.java
@@ -0,0 +1,30 @@
+package maingame;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * Created by Timo on 26.02.2017
+ */
+
+
+public class Graphics {
+    public static void main(String[] args) {
+        BufferedImage img = new BufferedImage(1280, 720, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = img.createGraphics();
+
+        for(int i = 0; i<=100; i++) {
+            g.fillRect(new Random().nextInt(1280), new Random().nextInt(720), 2, 2);
+        }
+
+        try {
+            ImageIO.write(img, "png", new File("D:/Output/test.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
