8ee260833fb7b4bae552086ea91a97a353e7e35c
Daniel <daniel-brormann@hotmail.com>
2017-02-26 20:38:47 +0100

    V2

--- src/maingame/MainFrame.java
+++ src/maingame/MainFrame.java
@@ -6,54 +6,12 @@ import java.awt.*;
 /**
  * Created by Daniel on 26.02.2017.
  */
-public class MainFrame extends JPanel {
-    String title;
-    JLabel text;
-    JFrame meinFrame;
-    Color c;
-    Button start;
-    Button info;
-    Button einstellungen;
-    Button beenden;
-    MainGame game;
-    int x;
-    int y;
-    int w;
-    int h;
+public class MainFrame extends JFrame {
 
-
-    public MainFrame(String title, String text, int x , int y, int w, int h, Color c, Color schriftfarbe , int schriftgröße , MainGame game )
-    {
-        setLayout(null);
-        this.title = title;
-        this.c = c;
-        //MainFrame
-        this.x =x;
-        this.y =y;
-        this.w =w;
-        this.h =h;
-        this.game = game;
-        game.frame.setBounds(x, y, w,h);
-
-        game.frame.setBackground(c);
-        start = new Button(250 , 50 , 100, 50 , Color.DARK_GRAY , "Start");
-        this.start=start;
-
-
-
-        this.text = new JLabel(text);
-        this.text.setBounds(100,10,600,30);
-        //this.text.setFont(new Font("1", Font.ITALIC, schriftgröße));
-        add(this.text);
+    MainFrame(){
 
 
     }
-    @Override
-    protected void paintComponent(Graphics g2) {
-        Graphics2D g = (Graphics2D) g2;
-        start.render(g);
-    }
-
 
 }
 
--- src/maingame/MainGame.java
+++ src/maingame/MainGame.java
@@ -7,23 +7,14 @@ import java.awt.*;
  * Created by Daniel on 26.02.2017.
  */
 public class MainGame {
-    JFrame frame;
-    MainFrame mainframe;
 
 
     public static void main(String[] args) {
-        new MainGame().start();
 
 
     }
 
-    void start() {
-        frame = new JFrame();
-        mainframe = new MainFrame("start","Herzlich Willkommen im Main Menu, bitte wähle eine Option.", 500, 200, 600, 600, Color.WHITE , Color.BLACK, 16, this);
-        frame.add(mainframe);
-        frame.setVisible(true);
 
-    }
 }
 
 
