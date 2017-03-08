3b2d85fdd861ef433f4cade16ff372cf5e2d455c
Daniel <daniel-brormann@hotmail.com>
2017-03-08 21:45:46 +0100

    eine Version die Funktioniert!!!

--- src/maingame/HauptSpiel.java
+++ src/maingame/HauptSpiel.java
@@ -16,20 +16,30 @@ public class HauptSpiel {
     int skip = 0;
     PictureButtons scherebutton;
     BufferedImage schere;
+    PictureButtons steinbutton;
+    BufferedImage stein;
+    PictureButtons papierbutton;
+    BufferedImage papier;
+    PictureButtons echsebutton;
+    BufferedImage echse;
+
 
 
     //Konstruktor
     HauptSpiel(MainGame game) {
         this.game = game;
         game.frame.setBounds(0, 0, 1920, 1080);
-        int xschere = 300;
-        int yschere = 300;
-        int wschere = 225;
-        int hschere = 225;
-        scherebutton = new PictureButtons(xschere, yschere, wschere, hschere);
-        //bild laden
+        scherebutton = new PictureButtons(300, 200, 225, 225);
+        steinbutton = new PictureButtons( 450,500,225,225);
+        papierbutton = new PictureButtons( 650,750,225,225);
+        echsebutton = new PictureButtons( 1000, 750, 225,225);
+
+        //bilder laden
         try {
             schere = ImageIO.read(new File("C:\\TestJavaGraphics\\Schere.png"));
+            stein = ImageIO.read(new File("C:\\TestJavaGraphics\\Stein.png"));
+            papier = ImageIO.read(new File("C:\\TestJavaGraphics\\Papier.png"));
+            echse = ImageIO.read(new File("C:\\TestJavaGraphics\\Echse1.png"));
         } catch (Exception e) {
         }
         game.frame.addMouseListener(new MouseListener() {
@@ -40,8 +50,18 @@ public class HauptSpiel {
                         if (skip == 0) {
                             skip++;
                         } else if (skip == 1) {
+                            //clickactions: vielleicht macht man dass sich das ausgewählte Bild in die Mitte verschieben oder auf eine seite.
                             if (scherebutton.buttonPointedpicture(e.getX(), e.getY())) {
                                 System.out.println("schere");
+                            }
+                            else if (steinbutton.buttonPointedpicture(e.getX(), e.getY())) {
+                                System.out.println("stein");
+                            }
+                            else if (papierbutton.buttonPointedpicture(e.getX(), e.getY())) {
+                                System.out.println("papier");
+                            }
+                            else if (echsebutton.buttonPointedpicture(e.getX(), e.getY())) {
+                                System.out.println("echse");
                             }
                         }
                 }
@@ -72,9 +92,6 @@ public class HauptSpiel {
 
     //rendermethode vom maingame
     public void renderHauptSpiel(Graphics2D g) {
-        //initialisierung scherebutton
-
-
         //Screen 1
         if (skip == 0) {
             g.setFont(new Font("Arial", Font.BOLD, 24));
@@ -85,9 +102,15 @@ public class HauptSpiel {
         //Screen 2
         if (skip == 1) {
             g.setFont(new Font("Arial", Font.BOLD, 20));
-            g.drawString("Wählen sie einen der Folgenden Begriffe.", 700, 200);
-            g.drawImage(schere, 300, 300, null);
+            g.drawString("Wählen sie einen der Folgenden Begriffe.", 700, 100);
+            g.drawImage(schere, 300, 200, null);
+            g.drawImage(stein, 450, 500,null);
+            g.drawImage(papier, 650,750,null);
+            g.drawImage(echse, 1000, 750, null);
             scherebutton.renderpicturebutton(g);
+            steinbutton.renderpicturebutton(g);
+            papierbutton.renderpicturebutton(g);
+            echsebutton.renderpicturebutton(g);
         }
     }
 
@@ -95,7 +118,10 @@ public class HauptSpiel {
     public void updateMethode(int x, int y) {
         try {
             if (skip == 1) {
-                scherebutton.buttonPointedpicture(x, y);
+                scherebutton.buttonPointedpicture( x, y);
+                steinbutton.buttonPointedpicture( x, y);
+                papierbutton.buttonPointedpicture(x,y );
+                echsebutton.buttonPointedpicture(x,y);
             }
         } catch (Exception e) {
      }
